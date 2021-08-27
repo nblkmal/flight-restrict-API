ffdfb84c225b27bb456040bb94d3d7f32aa06ee5
nblkmal <nblkmal@gmail.com>
2021-08-28 01:36:55 +0800

    hash pasword

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -19,13 +20,13 @@ class UserSeeder extends Seeder
                 'id' => 1,
                 'name' => 'Nabil',
                 'email' => 'nblkmal@gmail.com',
-                'password' => 'password'
+                'password' => Hash::make('password')
             ],
             [
                 'id' => 2,
                 'name' => 'Aqil',
                 'email' => 'aqilat94@gmail.com',
-                'password' => 'password'
+                'password' => Hash::make('password')
             ],
         ]);
     }
