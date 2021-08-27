27c0dc12bb69a1a853b0e979445839fd8cbe1b30
nblkmal <nblkmal@gmail.com>
2021-08-28 01:46:44 +0800

    create coordinate model

--- app/Models/Coordinate.php
+++ app/Models/Coordinate.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Place;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Coordinate extends Model
 {
@@ -14,4 +15,9 @@ class Coordinate extends Model
         'longitude',
         'place_id',
     ];
+
+    public function place()
+    {
+        return $this->belongsTo(Place::class, 'place_id');
+    }
 }
