7b66a70499eef4c0bcdb940ac1e074739cd470ab
nblkmal <nblkmal@gmail.com>
2021-09-04 22:33:56 +0800

    Donate : create Model -a, add migrations for amount, payment status and billcode

---
+++ database/factories/DonateFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Donate;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DonateFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Donate::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_09_04_141309_create_donates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDonatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignId('user_id')->on('users');
+            $table->integer('amount');
+            $table->boolean('payment_status')->default(false);
+            $table->string('toyyibPay_bill_code')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donates');
+    }
+}
