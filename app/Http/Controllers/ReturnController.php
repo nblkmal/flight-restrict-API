b17318e5e6334d6ca4d63f411c20cc8c1ea9aca6
nblkmal <nblkmal@gmail.com>
2021-09-07 12:23:13 +0800

    return-url : add compact

--- app/Http/Controllers/ReturnController.php
+++ app/Http/Controllers/ReturnController.php
@@ -10,11 +10,13 @@ class ReturnController extends Controller
     public function index(Request $request)
     {
         $donate = Donate::where('toyyibpay_bill_code', $request->billcode)->first();
+        $donater = $request->name;
+
         if($donate){ 
             if($donate->uuid.$donate->id == $request->order_id){
                 $donate->update(['payment_status'=>1]);
     
-                return view('donate.paid');
+                return view('donate.paid', compact('donater'));
             }
             return view('donate.try', compact('donate'));
         }
