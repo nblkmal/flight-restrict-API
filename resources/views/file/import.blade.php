f32772bf0ff3bf21174b3e5203d969cdbeac737f
nblkmal <nblkmal@gmail.com>
2021-09-07 15:15:23 +0800

    import blade

--- resources/views/file/import.blade.php
+++ resources/views/file/import.blade.php
@@ -1,31 +1,69 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Import Export Excel & CSV to Database in Laravel 7</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-</head>
-
-<body>
-    <div class="container mt-5 text-center">
-        <h2 class="mb-4">
-            Laravel 7 Import and Export CSV & Excel to Database Example
-        </h2>
-
-        <form action="{{ route('file-import') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="form-group mb-4" style="max-width: 500px; margin: 0 auto;">
-                <div class="custom-file text-left">
-                    <input type="file" name="file" class="custom-file-input" id="customFile">
-                    <label class="custom-file-label" for="customFile">Choose file</label>
+@section('content')
+<div class="container-fluid justify-content-center text-center">
+    <div class="row justify-content-center">
+        <div class="col-sm-8">
+            <div class="card">
+                <div class="card-body">
+                    <form method="POST" action="{{ route('file:export_states') }}">
+                        @csrf
+                        <button class="btn btn-primary">Export States</button>
+                    </form>
+                    {{-- <a href="{{ route('file:export_states') }}" class="btn btn-primary">Export States</a> --}}
                 </div>
             </div>
-            <button class="btn btn-primary">Import data</button>
-            {{-- <a class="btn btn-success" href="{{ route('file-export') }}">Export data</a> --}}
-        </form>
+        </div>
     </div>
-</body>
-
-</html>
+    <div class="row justify-content-center">
+        <div class="col-sm-8 ">
+            <div class="card">
+                <div class="card-body">
+                    @if (session('success'))
+                        <div class="alert alert-success">
+                            {!! session('success') !!}
+                        </div>
+                    @endif
+                    <h2 class="mb-2" style="color: #414141; font-family: poppins; font-weight: bold;">
+                        {{-- <i class="btn fas fa-angle-left fa-lg pr-4" onclick="window.location='{{ url()->previous() }}'"></i> --}}
+                    Upload File For Places</h2>
+                    <form action="{{ route('file:import_places') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group mb-4" style="max-width: 500px; margin: 0 auto;">
+                            <div class="custom-file text-left">
+                                <input type="file" name="file" class="custom-file-input" id="customFile">
+                                <label class="custom-file-label" for="customFile">Choose file</label>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary">Import data</button>
+                        {{-- <a class="btn btn-success" href="{{ route('file-export') }}">Export data</a> --}}
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-sm-8 ">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="mb-2" style="color: #414141; font-family: poppins; font-weight: bold;">
+                        {{-- <i class="btn fas fa-angle-left fa-lg pr-4" onclick="window.location='{{ url()->previous() }}'"></i> --}}
+                    Upload File For Coordinates</h2>
+                    {{-- <form action="{{ route('import:parliament') }}" method="POST" enctype="multipart/form-data"> --}}
+                        @csrf
+                        <div class="form-group mb-4" style="max-width: 500px; margin: 0 auto;">
+                            <div class="custom-file text-left">
+                                <input type="file" name="file" class="custom-file-input" id="customFile">
+                                <label class="custom-file-label" for="customFile">Choose file</label>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary">Import data</button>
+                        {{-- <a class="btn btn-success" href="{{ route('file-export') }}">Export data</a> --}}
+                    {{-- </form> --}}
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</div>
+@endsection
