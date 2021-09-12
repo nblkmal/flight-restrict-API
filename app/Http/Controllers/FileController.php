3413aafa88a6cc2f48bc9644b9e8d5a5941b0acb
nblkmal <nblkmal@gmail.com>
2021-09-13 00:44:37 +0800

    add import coordinates function

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Exports\PlacesExport;
 use App\Exports\StatesExport;
 use App\Imports\PlacesImport;
+use App\Imports\CoordinatesImport;
 use Maatwebsite\Excel\Facades\Excel;
 
 class FileController extends Controller
@@ -19,7 +20,13 @@ class FileController extends Controller
     public function places(Request $request)
     {
         Excel::import(new PlacesImport, $request->file('file')->store('temp'));
-        return back()->with('success', 'Succesfully Added');;
+        return back()->with('success', 'Succesfully Added');
+    }
+
+    public function coordinates(Request $request)
+    {
+        Excel::import(new CoordinatesImport, $request->file('file')->store('temp'));
+        return back()->with('success', 'Succesfully Added');
     }
 
     public function placesExport()
