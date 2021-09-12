a44834567efb69de8f616d386d034c3452fd1c2c
nblkmal <nblkmal@gmail.com>
2021-09-13 00:43:50 +0800

    add docs info

--- resources/views/docs/index.blade.php
+++ resources/views/docs/index.blade.php
@@ -25,6 +25,17 @@
         border: none;
         color: #ffffff
     }
+
+    kbd {
+        background-color: #393E46 !important;
+        padding: 0.5rem 0.5rem !important;
+    }
+
+    .link {
+        /* border: #393E46 solid 1px !important; */
+        background-color: #e2e2e2;
+        padding: 0.3rem !important;
+    }
 </style>
 @section('content')
 <div class="container">
@@ -37,7 +48,7 @@
                 <div class="card-body p-0 ">
                     <div class="row method m-3 align-items-center">
                         <button class="btn px-5">GET</button>
-                        <input class="ml-4" type="text" id="url-places" value="api/v1/places" readonly>
+                        <input class="ml-4" type="text" id="url-places" value="api/v1/getPlaces" readonly>
                         <i class='bx bxs-copy box--end mr-4' onclick="myFunction('url-places')" data-toggle="tooltip" data-placement="top" title="Copy" type="button" style='color:#ffffff'  ></i>
                     </div>
                 </div>
@@ -46,12 +57,34 @@
                 <div class="card-body p-0 ">
                     <div class="flex-row method m-3 align-items-center">
                         <button class="btn px-5">GET</button>
-                        <input class="ml-4" type="text" id="url-coords" value="api/v1/coordinates" readonly>
+                        <input class="ml-4" type="text" id="url-coords" value="api/v1/getCoordinates" readonly>
                         <i class='bx bxs-copy box--end mr-4' onclick="myFunction('url-coords')" data-toggle="tooltip" data-placement="top" title="Copy" type="button" style='color:#ffffff'  ></i>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-sm-10">
+            <div class="card">
+                <div class="card-body">
+                    
+                    <p>For Laravel usage, please get an API Token to get your personal token.</p>
+                    <p>Refer to this documentation <a class="link rounded-link" href="https://laravel.com/docs/8.x/http-client">Laravel HTTP Client</a></p>
+                    <p><kbd>$response = Http::get('http://example.com');</kbd></p>
+                    <p>
+                        <kbd>
+                            $response = Http::withHeaders([
+                                'Accept' => 'application/json',
+                                'Authorization' => 'Bearer <--Personal Access Token-->'
+                            ])
+                        </kbd>
+                    </p>
+                    
+                </div>
+            </div>
+        </div>
+        
     </div>
     
     <script>
