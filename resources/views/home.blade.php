ce86b4e32f26a9cbab701e2312decd01aafb3989
nblkmal <nblkmal@gmail.com>
2021-09-19 14:24:23 +0800

    update UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,8 @@
     }
 </style>
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
+<div class="container-fluid">
+    <div class="row border justify-content-center">
         <div class="col-md-9">
             <div class="my-2" style="font-family: 'Poppins', sans-serif;">
                 {{-- Guna permission --}}
@@ -16,41 +16,55 @@
 
                 @else
                     <h2>Hi {{ auth()->user()->name ?? 'everyone' }} ! Welcome to Flight Restriction API</h2>
-                    <small>This is normal user</small>
+                    {{-- <small>This is normal user</small> --}}
                 @endrole
+                
             </div>
-            <div>
-                <h4 class="text-center my-3" style="font-family: 'Poppins', sans-serif;">About this API</h4>
-                <div class="row justify-content-center my-3">
-                    <div class="card px-3">
-                        <div class="card-body text-center">
-                            <div class="d-inline-flex align-items-center">
-                                <i class='bx bxs-map-alt bx-lg mr-3' style='color:#434b57'></i>Collection of all flight restrict area in Malaysia
-                            </div>
-                        </div>
+        </div>
+    </div>
+</div>
+<div class="container border">
+    <h4 class="text-center my-3" style="font-family: 'Poppins', sans-serif;">About this API</h4>
+        <div class="row justify-content-center my-3">
+            <div class="col-sm-4 card px-3 m-2">
+                <div class="card-body text-center">
+                    <div class="d-inline-flex align-items-center">
+                        <i class='bx bxs-map-alt bx-lg mr-3' style='color:#434b57'></i>Collection of all flight restrict area in Malaysia
                     </div>
                 </div>
-                <div class="row justify-content-center my-3">
-                    <div class="card px-3">
-                        <div class="card-body text-center">
-                            <div class="d-inline-flex align-items-center">
-                                <i class='bx bxs-pin bx-lg mr-3' style='color:#434b57' ></i>Point coordinates of each places
-                            </div>
-                        </div>
+            </div>
+            <div class="col-sm-4 card px-3 m-2">
+                <div class="card-body text-center">
+                    <div class="d-inline-flex align-items-center">
+                        <i class='bx bxs-pin bx-lg mr-3' style='color:#434b57' ></i>Point coordinates of each places
                     </div>
                 </div>
-                <div class="row justify-content-center my-3">
-                    <div class="card px-3">
-                        <div class="card-body text-center">
-                            <div class="d-inline-flex align-items-center">
-                                <i class='bx bx-shape-polygon bx-lg mr-3' style='color:#434b57' ></i>Polygon coordinates of each places
-                            </div>
-                            
-                        </div>
+            </div>
+            <div class="col-sm-4 card px-3 m-2">
+                <div class="card-body text-center">
+                    <div class="d-inline-flex align-items-center">
+                        <i class='bx bx-shape-polygon bx-lg mr-3' style='color:#434b57' ></i>Polygon coordinates of each places
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+        {{-- <div class="row justify-content-center my-3">
+            <div class="card px-3">
+                <div class="card-body text-center">
+                    <div class="d-inline-flex align-items-center">
+                        <i class='bx bxs-pin bx-lg mr-3' style='color:#434b57' ></i>Point coordinates of each places
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center my-3">
+            <div class="card px-3">
+                <div class="card-body text-center">
+                    <div class="d-inline-flex align-items-center">
+                        <i class='bx bx-shape-polygon bx-lg mr-3' style='color:#434b57' ></i>Polygon coordinates of each places
+                    </div>
+                </div>
+            </div>
+        </div> --}}
 </div>
 @endsection
