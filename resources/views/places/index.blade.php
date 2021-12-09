2f66d5de146f9cce2e8f134c8b9c1436838ec4e5
nblkmal <nblkmal@gmail.com>
2021-12-09 10:35:37 +0800

    add button to download geojson

--- resources/views/places/index.blade.php
+++ resources/views/places/index.blade.php
@@ -5,6 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <h2>Places Data</h2>
+            <a href="{{ route('coordinates:export-geojson') }}" class="btn btn-primary">Export Geojson Coordinates</a>
             <div class="card">
                 <div class="card-body">
                     {!! $dataTable->table() !!}
