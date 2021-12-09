31ec0934fe021b99d9c839b3428f77f2a0858d3a
nblkmal <nblkmal@gmail.com>
2021-12-09 10:36:05 +0800

    add geoJsonExport function

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -2,13 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Coordinate;
 use Illuminate\Http\Request;
 use App\DataTables\PlaceDataTable;
+use Illuminate\Support\Facades\Storage;
 
 class PlaceController extends Controller
 {
     public function index(PlaceDataTable $dataTable)
     {
         return $dataTable->render('places.index');
+    }
+
+    public function geoJsonExport()
+    {
+        $coordinates = Coordinate::with('place.type')->get();
+        
+        $features = array();
+        foreach($coordinates as $key => $coordinate)
+        {
+            $features[] = array(
+                'type' => 'Feature',
+                'properties' => array('name' => $coordinate->place->name),
+                'geometry' => array(
+                    'type' => $coordinate->place->type->name,
+                    'coordinates' => [
+                        $coordinate->longitude, $coordinate->latitude
+                    ],
+                ),
+            );
+        }
+
+        $geojson = array(
+            'type' => 'FeatureCollection',
+            'features' => $features,
+        );
+
+        $encoded = json_encode($geojson);
+        $fileName = time() . '_datafile.json';
+
+        // need to mention which disk if using storage
+        $test = Storage::disk('public')->put('storage/upload/json/'.$fileName, $encoded);
+
+        return Storage::disk('public')->download('storage/upload/json/'.$fileName);
     }
 }
